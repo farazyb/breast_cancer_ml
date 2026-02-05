7f33a07b8bf1c077266f906623de6df77becb98e
faraz yazdani <farazyazdanib@gmail.com>
2026-02-05 13:17:23 +0100

    init

---
+++ docs/docs/index.md
@@ -0,0 +1,10 @@
+# breast_cancer documentation!
+
+## Description
+
+Develop a machine learning model to accurately classify breast cancer cases as malignant or benign.
+
+## Commands
+
+The Makefile contains the central entry points for common tasks related to this project.
+
---
+++ notebooks/script/01-Business_Understanding.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Breast Cancer
+# ## description:
+# The Breast Cancer dataset [1] is a widely used dataset for learning and practicing machine learning techniques. It contains diagnostic data for breast cancer cases, including features computed from digitized images of a fine needle aspirate (FNA) of a breast mass. These features describe characteristics of the cell nuclei, such as radius, texture, and smoothness, and are compiled into a convenient dataset.
+# ## Goal:
+# Develop a machine learning model to accurately classify breast cancer cases as malignant or benign.
+# ## Citations:
+# [1] Street, W. N., Wolberg, W. H., & Mangasarian, O. L. (1993). Nuclear feature extraction for breast tumor diagnosis. IS&T/SPIE International Symposium on Electronic Imaging: Science and Technology. Retrieved from https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Diagnostic)
+
+# ## Business Understanding
+# ### Problem
+# * Diagnosis of breast tumors is traditionally done with a complete biopsy, which is invasive.
+# * FNA (fine needle aspiration) is less invasive, but diagnosis is not always successful and remains highly dependent on the skill and experience of the physician, meaning it is a subjective process.
+# * So the main question: how to make FNA diagnosis faster, more accurate, and more objective so that it is less dependent on individual judgment.
